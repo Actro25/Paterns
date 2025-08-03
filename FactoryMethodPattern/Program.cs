b823c26996d55679b4d33d426fa8fb1275e48c89
Vergil <skiril502@gmail.com>
2025-08-03 18:10:56 +0300

    learn new patern Factory Method Pattern

---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿abstract class INotification 
+{
+    abstract public void Send(string message);
+}
+class EmailNotification : INotification { public override void Send(string message) => Console.WriteLine($"Email sent: {message}"); }
+class SMSNotification : INotification { public override void Send(string message) => Console.WriteLine($"SMS sent: {message}"); }
+class PushNotification : INotification { public override void Send(string message) => Console.WriteLine($"Push sent: {message}"); }
+class NotificationFactory {
+    private static NotificationFactory _instance;
+    private static Object _lock = new Object();
+    private NotificationFactory() { }
+    public static NotificationFactory Instance 
+    { 
+        get
+        {
+            lock (_lock) {                 
+                if (_instance == null) 
+                {
+                    _instance = new NotificationFactory();
+                }
+                return _instance;
+            }
+        } 
+    }
+    public INotification CreateNotification(string type) {
+        return type switch {
+            "email" => new EmailNotification(),
+            "sms" => new SMSNotification(),
+            "push" => new PushNotification(),
+            _ => throw new ArgumentException("Invalid notification type")
+        };
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var type = Console.ReadLine();
+        NotificationFactory.Instance.CreateNotification(type).Send(Console.ReadLine());
+    }
+}
